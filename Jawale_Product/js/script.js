0504fbad4e3ab91e0451f9e85f599e02dbe93887
lokeshjawale1996 <jawalelokesh2411@gmail.com>
2023-01-17 20:00:13 +0530

    project updated

--- Jawale_Product/js/script.js
+++ Jawale_Product/js/script.js
@@ -14,6 +14,7 @@ const product = document.querySelector('.product');
 const showProduct = document.querySelector('showProduct')
 // const images = ['./images/papd.png','./images/kurdai2.png','./images/papd2.png'];
 // Toggle Mobile Menu
+
 function navToggle() {
     btn.classList.toggle('open')
     menu.classList.toggle('flex')
@@ -31,89 +32,43 @@ const wait = function (seconds) {
 ///////////////////////////////
 
 //Create a function 'createImage' which receives imgPath as an input.
-const createImage = function (imgPath) {
-  return new Promise(function (resolve, reject) {
-    // This function returns a promise which creates a new image (use document.createElement('img'))
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     // This function returns a promise which creates a new image (use document.createElement('img'))
 
-    const img = document.createElement('img');
-    // and sets the .src attribute to the provided image path.
+//     const img = document.createElement('img');
+//     // and sets the .src attribute to the provided image path.
 
-    img.src = imgPath;
+//     img.src = imgPath;
 
-    //append it to the DOM element with the 'images' class and resolve the promise
-    img.addEventListener('load', function () {
-      imgContainer.append(img);
-      resolve(img);
-    });
+//     //append it to the DOM element with the 'images' class and resolve the promise
+//     img.addEventListener('load', function () {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
 
-    //In case there is an error loading the image ('error' event), reject the promise.
-    img.addEventListener('error', function () {
-      reject(new Error('Image not found'));
-    });
-  });
-};
+//     //In case there is an error loading the image ('error' event), reject the promise.
+//     img.addEventListener('error', function () {
+//       reject(new Error('Image not found'));
+//     });
+//   });
+// };
 
-let currentImg;
+// let currentImg;
 
-setInterval(function () {}, 1000);
-
-
-createImage('./images/bibde.jpg')
-  .then(img => {
-    currentImg = img;
-    console.log('Image 1 loaded');
-    //After the image has loaded, pause execution for 2 seconds using the wait function
-    // return wait(2);
-    //After the 2 seconds have passed,
-  })
-  // .then(() => {s
-  //   //hide the current image (set display to 'none'),
-  //   currentImg.style.display = 'none';
-  //   return createImage('./images/kurdai2.png');
-  // })
-  // .then(img2 => {
-  //   currentImg = img2;
-
-  //   console.log('Image 2 loaded');
-  //   return wait(2);
-  // })
-  // .then(() => {
-  //   currentImg.style.display = 'none';
-  //   return createImage('./images/papdj.png');
-  // })
-  // .then(img3 => {
-  //   currentImg = img3;
-
-  //   console.log('Image 3 loaded');
-  //   return ;
-  // })
- .catch(err => console.error(err));
+// setInterval(function () {}, 1000);
 
 
-//  About us
+// createImage('./images/bibde.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
+//     //After the image has loaded, pause execution for 2 seconds using the wait function
+//     // return wait(2);
+//     //After the 2 seconds have passed,
+//   })
+//   .catch(err => console.error(err));
 
-// aboutClick.addEventListener('click',function(){
-
-//   if(about.classList.contains('hidden')){
-//     about.classList.remove('hidden');
-//     heroCont.classList.add("hidden");
-//     statsCont.classList.add("hidden");
-//     fetauresCont.classList.add("hidden");
-//     ctaCont.classList.add("hidden");
-//     imgContainer.classList.add("hidden")
-//     console.log("about btn clicked");
-//   }
-//   else{
-//     about.classList.add('hidden');
-//     heroCont.classList.remove("hidden");
-//     statsCont.classList.remove("hidden");
-//     fetauresCont.classList.remove("hidden");
-//     ctaCont.classList.remove("hidden");
-//     imgContainer.classList.remove("hidden")
-//   }
-   
-
-// });
 function currentTime(){
   let date = new Date(); 
   let hh = date.getHours();
@@ -144,23 +99,64 @@ currentTime()
 
 
 
-// product page
-// function navToggle2() {
-//   productSec.classList.toggle('open')
-//   productSec.classList.toggle('hidden')
-// }
-// showProduct.addEventListener('click', navToggle2)
+// Temporary close Products section
+var tempCloseProducts = document.querySelector(".tempCloseProducts");
+    var productsContent = document.querySelector(".prodContent");
+    tempCloseProducts.addEventListener("click", function () {
+      if (productsContent.classList.contains("hidden")) {
+        productsContent.classList.remove("hidden");
+      } else {
+        productsContent.classList.add("hidden");
+      }
+    });
 
-// showProduct.addEventListener('click',function(){
+    // Temporary close Features section
+    var tempCloseFeatures = document.querySelector(".tempCloseFeatures");
+    var featuresContent = document.querySelector(".featuresContent");
+    tempCloseFeatures.addEventListener("click", function () {
+      if (featuresContent.classList.contains("hidden")) {
+        featuresContent.classList.remove("hidden");
+      } else {
+        featuresContent.classList.add("hidden");
+      }
+    });
 
-//   if(productSec.classList.contains('hidden')){
-//     productSec.classList.remove('hidden');
-    
-//   }else{
-//     productSec.classList.add('hidden');
-   
+    // Temperary close about section
+    var tempCloseAbout = document.querySelector(".tempCloseAbout");
+    var aboutContent = document.querySelector(".aboutContent");
+    tempCloseAbout.addEventListener("click", function () {
+      if (aboutContent.classList.contains("hidden")) {
+        aboutContent.classList.remove("hidden");
+      } else {
+        aboutContent.classList.add("hidden");
+      }
+    });
 
-//   }
- 
-  
-// })
+    // showing date and day on screen
+    var date1= document.querySelector('.date');
+    var day1= document.querySelector('.day');
+
+    const date = new Date()
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+// This arrangement can be altered based on how we want the date's format to appear.
+
+var a = new Date();
+        var weekdays = new Array(7);
+        weekdays[0] = "Sunday";
+        weekdays[1] = "Monday";
+        weekdays[2] = "Tuesday";
+        weekdays[3] = "Wednesday";
+        weekdays[4] = "Thursday";
+        weekdays[5] = "Friday";
+        weekdays[6] = "Saturday";
+        var r = weekdays[a.getDay()];
+
+  let currentDate = `${day}-${month}-${year}`;
+
+  r = r.slice(0,3);
+date1.innerHTML=currentDate;
+day1.innerHTML= r;
